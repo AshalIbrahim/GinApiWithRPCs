0e15526cda6d94d11624adb8ea0d872ba47f3367
Ashal Ibrahim Khan <113935461+AshalIbrahim@users.noreply.github.com>
2025-06-22 15:11:43 +0500

    Create grpc_main.go

---
+++ grpc_main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	ginSwagger "github.com/swaggo/gin-swagger"
+	swaggerFiles "github.com/swaggo/files"
+
+	"google.golang.org/grpc"
+	userpb "github.com/AshalIbrahim/GinApiWithRPCs/proto/userpb" // adjust path as needed
+)
+
+var grpcClient userpb.UserServiceClient
+
+func initGRPCClient() {
+	conn, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("Failed to connect to gRPC server: %v", err)
+	}
+	grpcClient = userpb.NewUserServiceClient(conn)
+	log.Println("Connected to gRPC server.")
+}
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	initGRPCClient()
+
+	r := gin.Default()
+
+	api := r.Group("/api")
+	{
+		v1 := api.Group("/v1")
+		{
+			v1.GET("/users", AllUsers)
+			v1.POST("/users", createUser)
+			v1.PUT("/users/:id", updateUser)
+			v1.DELETE("/users/:id", deleteUser)
+		}
+		v2 := api.Group("/v2")
+		{
+			v2.GET("/", V2api)
+		}
+	}
+
+	r.StaticFile("/swagger-ui", "./swagger-ui.html")
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+
+	r.Run(":8080")
+}
